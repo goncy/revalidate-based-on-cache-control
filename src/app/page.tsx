d236dda9a852fb3adcc14218149f61bbf8b25743
goncy <gonzalo.pozzo4@gmail.com>
2024-03-27 09:28:05 -0300

    Set baseurl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { revalidatePath } from "next/cache";
 
+const BASE_URL = process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : "http://localhost:3000"
+
 export default async function HomePage() {
-  const revalidation = await fetch('http://localhost:3000/revalidation')
+  const revalidation = await fetch(BASE_URL + '/revalidation')
   const time = revalidation.headers.get('cache-control')!.split('=')[1]
   const data = await revalidation.text()
 
-  await fetch('http://localhost:3000/revalidation', { next: {revalidate: Number(time)} })
+  await fetch(BASE_URL + '/revalidation', { next: {revalidate: Number(time)} })
 
   async function revalidate() {
     'use server'
